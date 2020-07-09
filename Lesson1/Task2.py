7ec0372377b843049709c7eee73840613c9eeaa4
DenisPt <ptakhov@gmail.com>
2020-07-09 23:07:59 +1000

    Исправил формат вывода

--- Lesson1/Task2.py
+++ Lesson1/Task2.py
@@ -1,3 +1,3 @@
 # Task2
 sec = int(input("Input few seconds: "))
-print(f"{sec // (60 * 60)}:{(sec % (60 * 60)) // 60}:{sec % 60}")
+print(f"{sec // (60 * 60):02d}:{(sec % (60 * 60)) // 60:02d}:{sec % 60:02d}")
