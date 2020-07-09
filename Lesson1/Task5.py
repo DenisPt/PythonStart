c4e14e8f0f6819e5b98910680b16c0c4f76d1506
DenisPt <ptakhov@gmail.com>
2020-07-09 22:29:33 +1000

    исправил синтаксис

--- Lesson1/Task5.py
+++ Lesson1/Task5.py
@@ -2,12 +2,12 @@
 rev = int(input('Input your revenue: '))
 costs = int(input('Input your costs: '))
 prof = rev - costs
-if rev = costs:
+if rev == costs:
     print('Your rev = costs')
 elif rev > costs:
     print(f'Your profit = {prof}')
     print(f'Your profitability = {prof / rev}')
     emp = int(input('Input your number of employees: '))
-    print(f'Your profit per employee = {emp / prof}')
+    print(f'Your profit per employee = {prof / emp}')
 else:
     print(f'Your loss = {-prof}')
--- Lesson1/Task6.py
+++ Lesson1/Task6.py
@@ -2,7 +2,7 @@
 x = float(input('Input positive number: '))
 y = float(input("Input other positive number: "))
 i = 1
-while x < b
+while x < y:
     x *= 1.1
     i += 1
 print(f"Num of day = {i}")
