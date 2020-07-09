09d630c6205c805f1c0338ca55138d017238acd3
DenisPt <ptakhov@gmail.com>
2020-07-09 23:10:05 +1000

    Добавил проверку на 9

--- Lesson1/Task4.py
+++ Lesson1/Task4.py
@@ -4,6 +4,8 @@ num = int(input("Please, input very very big number: "))
 while num != 0:
     if num2 < (num % 10):
         num2 = num % 10
+    if num2 == 9:
+        break
     num //= 10
 print(f"Biggest digit in a very very big number = {num2}")
 
